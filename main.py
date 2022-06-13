08718d5c4e5123ad5f111fc4770905432a95763f
Ilqar Rasulov <ilgarrasulow@yandex.ru>
2022-06-13 14:44:11 +0200

    added tts and photo/video recording

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import tkinter as tk
 import json
 from tello import Tello
 from os.path import exists
+from os import makedirs
 from PIL import ImageTk, Image
 import cv2
 import jetson.inference
 import jetson.utils
 import numpy as np
 from tkinter import messagebox
+from tkinter import filedialog
+from datetime import datetime
+import threading
+from time import sleep
+import pyttsx3
 
+settings_track_objects_value=None
+with_detections_value = None
+frame_resolution=None
+
+last_frame=None
+
+recording=False
 settings_window_open=False
 settingsWindow=None
 tello_connected=False
@@ -19,8 +32,15 @@ lmain = None
 tello_status=None
 settings_data=None
 detection_object=None
+recording_label=None
+recording_label_text = None
+track_objects=False
 
- 
+net = None
+path_pictures="./media/pictures/"
+path_videos="./media/videos/"
+
+
 pid=[]
 pError=0
 
@@ -30,6 +50,10 @@ angle_step=0
 direction=0
 
 root = None
+
+tts_engine=pyttsx3.init()
+tts_engine.setProperty('rate',120)
+tts_engine.setProperty('voice', 'english_rp+f3')
 
 def on_settings_closing():
 	global root
@@ -57,6 +81,7 @@ def write_settings(data):
 def connect_to_tello(tello_ip_value):
 	
 	global drone
+	global frame_resolution
 	res = False
 	try:
 		if (tello_ip_value=="192.168.10.1"):
@@ -69,6 +94,8 @@ def connect_to_tello(tello_ip_value):
 		if res == True:
 			drone.connect()
 			drone.stream_on()
+			frame_resolution=(960,720)
+			drone.send_command("downvision 0")
 		
 	except:
 		
@@ -80,10 +107,12 @@ def connect_to_tello(tello_ip_value):
 def settings_on_save(data):
 	global settingsWindow
 	global settings_window_open
+	global settings_track_objects_value
 	data_to_save={}
 	try:
 		data_to_save["tello_ip"]=data["tello_ip"].get()
 		data_to_save["object_to_track"]=data["detection_object"].get()
+		data_to_save["custom_model_path"]=data["custom_model_path"].get()
 		
 		if (data["p_value"].get().strip()==""):
 			data_to_save["p"]=0.0
@@ -109,7 +138,14 @@ def settings_on_save(data):
 			data_to_save["angle_step"]=0
 		else:
 			data_to_save["angle_step"]=int(data["angle_step"].get().strip())
-	
+
+		
+		#print(settings_track_objects_value.get())
+		if settings_track_objects_value.get()==1:
+			data_to_save["track_objects"]=True
+		else:
+			data_to_save["track_objects"]=False
+			
 		write_settings(data_to_save)
 		messagebox.showinfo('Jetson Tools', 'Settings saved!')
 	
@@ -120,6 +156,13 @@ def settings_on_save(data):
 
 def onControlPanel():
 	global root
+	global recording_label
+	global recording_label_text
+	global with_detections_value
+	
+	recording_label_text = tk.StringVar()
+	
+	with_detections_value = tk.IntVar()
 	
 	# Toplevel object which will
 	# be treated as a new window
@@ -130,7 +173,7 @@ def onControlPanel():
 	controlWindow.title("Control Panel")
 
 	# sets the geometry of toplevel
-	controlWindow.geometry("670x130")
+	controlWindow.geometry("800x270")
 	
 	up_button = tk.Button(controlWindow, text="Up", command=lambda: send_command("up"))
 	up_button.grid(column=1, row=0, sticky=tk.NW, padx=5, pady=5)
@@ -165,7 +208,41 @@ def onControlPanel():
 	back_button = tk.Button(controlWindow, text="Back", command=lambda: send_command("back"))
 	back_button.grid(column=5, row=2, sticky=tk.NW, padx=5, pady=5)
 	
+	#################################
 	
+	flip_forward_button = tk.Button(controlWindow, text="Flip Forward", command=lambda: send_command("flip_f"))
+	flip_forward_button.grid(column=1, row=3, sticky=tk.NW, padx=5, pady=5)
+	
+	flip_left_button = tk.Button(controlWindow, text="Flip left", command=lambda: send_command("flip_l"))
+	flip_left_button.grid(column=0, row=4, sticky=tk.NW, padx=5, pady=5)
+	
+	flip_right_button = tk.Button(controlWindow, text="Flip right", command=lambda: send_command("flip_r"))
+	flip_right_button.grid(column=2, row=4, sticky=tk.NW, padx=5, pady=5)
+		
+	flip_backward_button = tk.Button(controlWindow, text="Flip Backward", command=lambda: send_command("flip_b"))
+	flip_backward_button.grid(column=1, row=5, sticky=tk.NW, padx=5, pady=5)
+	
+	################################
+	
+	take_picture_button = tk.Button(controlWindow, text="Take Picture", command=lambda: send_command("take_pic"))
+	take_picture_button.grid(column=3, row=3, sticky=tk.NW, padx=5, pady=5)
+	
+	start_video_button = tk.Button(controlWindow, text="Start Video", command=lambda: send_command("video_start"))
+	start_video_button.grid(column=3, row=4, sticky=tk.NW, padx=5, pady=5)
+	
+	with_detections_entry = tk.Checkbutton(controlWindow,text="with Detections",variable=with_detections_value,onvalue=1, offvalue=0)
+	with_detections_entry.grid(column=4, row=4, sticky=tk.NW, padx=5, pady=5)
+		
+	stop_video_button = tk.Button(controlWindow, text="Stop Video", command=lambda: send_command("video_stop"))
+	stop_video_button.grid(column=3, row=5, sticky=tk.NW, padx=5, pady=5)
+	
+	#################################
+	
+	recording_label = tk.Label(controlWindow, fg='red', textvariable=recording_label_text)
+	recording_label.grid(column=4, row=5, sticky=tk.NW, padx=5, pady=5)
+	
+	
+	###################################
 	img3 = tk.PhotoImage(file='./assets/logo3.png')
 	controlWindow.tk.call('wm', 'iconphoto', controlWindow._w, img3)
 
@@ -178,6 +255,9 @@ def onSettings():
 	global default_tello_udp_port
 	global settingsWindow
 	global settings_window_open
+	global settings_track_objects_value
+	
+	settings_track_objects_value = tk.IntVar()
 	
 	if not settings_window_open:
 		# Toplevel object which will
@@ -189,7 +269,7 @@ def onSettings():
 		settingsWindow.title("Settings")
 
 		# sets the geometry of toplevel
-		settingsWindow.geometry("380x270")
+		settingsWindow.geometry("500x350")
 	 
 		# A Label widget to show in toplevel
 		root.resizable(0, 0)
@@ -246,8 +326,16 @@ def onSettings():
 		angle_step_entry = tk.Entry(settingsWindow)
 		angle_step_entry.grid(column=1, row=6, sticky=tk.NW, padx=5, pady=5)
 		
-		restart_reminder_label = tk.Label(settingsWindow,text="Restart app after changes", fg='red')
-		restart_reminder_label.grid(column=0, row=7, sticky=tk.NW, padx=5, pady=5)
+		custom_model_path_label = tk.Label(settingsWindow, text="Custom Model path:")
+		custom_model_path_label.grid(column=0, row=7, sticky=tk.NW, padx=5, pady=5)
+
+		custom_model_path_entry = tk.Entry(settingsWindow)
+		custom_model_path_entry.grid(column=1, row=7, sticky=tk.NW, padx=5, pady=5)
+
+		track_object_entry = tk.Checkbutton(settingsWindow,text="Track Objects",variable=settings_track_objects_value,onvalue=1, offvalue=0)
+		track_object_entry.grid(column=0, row=8, sticky=tk.NW, padx=5, pady=5)
+		
+
 		
 		entry_fields={
 			"tello_ip":tello_ip_entry,
@@ -256,13 +344,21 @@ def onSettings():
 			"i_value":i_value_entry,
 			"d_value":d_value_entry,
 			"move_step":move_step_entry,
-			"angle_step":angle_step_entry
+			"angle_step":angle_step_entry,
+			"custom_model_path":custom_model_path_entry
 			
 		}
+		custom_model_path_button=tk.Button(settingsWindow,text="Choose Dialog", command =lambda: pick_Model(entry_fields))
+		custom_model_path_button.grid(column=2, row=7, sticky=tk.NW, padx=5, pady=5)
+		
+		restart_reminder_label = tk.Label(settingsWindow,text="Restart app after changes", fg='red')
+		restart_reminder_label.grid(column=0, row=10, sticky=tk.NW, padx=5, pady=5)
+		
+		
 		
 		
 		save_button = tk.Button(settingsWindow, text="Save", command=lambda: settings_on_save(entry_fields))
-		save_button.grid(column=1, row=7, sticky=tk.NW, padx=5, pady=5)
+		save_button.grid(column=1, row=10, sticky=tk.NW, padx=5, pady=5)
 		
 		
 		settings_read=read_settings()
@@ -271,27 +367,69 @@ def onSettings():
 			# set default values
 			tello_ip_entry.delete(0, tk.END)
 			tello_ip_entry.insert(0, default_tello_ip)
+			detection_object_entry.delete(0, tk.END)
+			detection_object_entry.insert(0, "person")
 			move_step_entry.delete(0, tk.END)
-			move_step_entry.insert(0, 0)
+			move_step_entry.insert(0, 30)
 			angle_step_entry.delete(0, tk.END)
-			angle_step_entry.insert(0, 0)
+			angle_step_entry.insert(0, 40)
+			custom_model_path_entry.delete(0, tk.END)
+			custom_model_path_entry.insert(0, "")
+			
+			p_value_entry.delete(0, tk.END)
+			p_value_entry.insert(0,0.3)
+			d_value_entry.delete(0, tk.END)
+			d_value_entry.insert(0,0.2)
 		else:
 			# set values from settings
 			tello_ip_entry.delete(0, tk.END)
-			tello_ip_entry.insert(0, settings_data["tello_ip"])
+			if "tello_ip" in settings_data:
+				tello_ip_entry.insert(0, settings_data["tello_ip"])
+			else:
+				tello_ip_entry.insert(0, default_tello_ip)
+				
 			detection_object_entry.delete(0, tk.END)
-			detection_object_entry.insert(0, settings_data["object_to_track"])
+			if "object_to_track" in settings_data:
+				detection_object_entry.insert(0, settings_data["object_to_track"])
+			else:
+				detection_object_entry.insert(0, "person")
+				
 			p_value_entry.delete(0, tk.END)
-			p_value_entry.insert(0, settings_data["p"])
+			if "p_value_entry" in settings_data:
+				p_value_entry.insert(0, settings_data["p"])
+			else:
+				p_value_entry.insert(0,0.3)
+				
 			i_value_entry.delete(0, tk.END)
-			i_value_entry.insert(0, settings_data["i"])
+			if "i_value_entry" in settings_data:
+				i_value_entry.insert(0, settings_data["i"])
+			else:
+				i_value_entry.insert(0,0)
 			d_value_entry.delete(0, tk.END)
-			d_value_entry.insert(0, settings_data["d"])
+			if "d_value_entry" in settings_data:
+				d_value_entry.insert(0, settings_data["d"])
+			else:
+				d_value_entry.insert(0,0.2)
 			move_step_entry.delete(0, tk.END)
-			move_step_entry.insert(0, settings_data["move_step"])
+			if "move_step" in settings_data:
+				move_step_entry.insert(0, settings_data["move_step"])
+			else:
+				move_step_entry.insert(0, 30)
 			angle_step_entry.delete(0, tk.END)
-			angle_step_entry.insert(0, settings_data["angle_step"])
-		
+			if "angle_step" in settings_data:
+				angle_step_entry.insert(0, settings_data["angle_step"])
+			else:
+				angle_step_entry.insert(0, 40)
+			custom_model_path_entry.delete(0, tk.END)
+			if "custom_model_path" in settings_data:
+				custom_model_path_entry.insert(0, settings_data["custom_model_path"])
+			else:
+				custom_model_path_entry.insert(0, "")
+			
+				
+			if "track_objects" in settings_data:
+				if settings_data["track_objects"]:
+					track_object_entry.select()
 		
 		# set on close event
 		settingsWindow.protocol("WM_DELETE_WINDOW", on_settings_closing)
@@ -299,6 +437,21 @@ def onSettings():
 		img3 = tk.PhotoImage(file='./assets/logo3.png')
 		settingsWindow.tk.call('wm', 'iconphoto', settingsWindow._w, img3)
 		settings_window_open=True
+
+def create_media_folder():
+	global path_pictures
+	global path_videos
+	
+	if not exists(path_pictures):
+		makedirs(path_pictures)
+	
+	if not exists(path_videos):
+		makedirs(path_videos)
+		
+def pick_Model(entry_fields):
+	folder_selected = filedialog.askdirectory()
+	entry_fields["custom_model_path"].delete(0, tk.END)
+	entry_fields["custom_model_path"].insert(0, folder_selected)
 
 # this function receives drone instance, information about center coordinates (x,y) of person, pid k values
 # and the last error value, calculas yaw speed and new error, sends this yaw speed to drone (or stop movement
@@ -318,18 +471,22 @@ def trackPerson(myDrone, center, w, pid,pError):
 	return error
 
 def send_command_js(yaw):
-    global drone
-    drone.joystick_control(0,0,0,0)
-    drone.joystick_control(0,0,yaw,0)
+	global drone
 
+	drone.joystick_control(0,0,0,0)
+	drone.joystick_control(0,0,yaw,0)
+
+		
 def send_command(command):
 	global drone
 	global move_step
 	global angle_step
 	
 	if (command=="takeoff"):
+		speak("Taking off")
 		drone.takeoff()
 	elif (command=="land"):
+		speak("Landing")
 		drone.land()
 	elif (command=="up"):
 		drone.move_up(move_step)
@@ -349,12 +506,97 @@ def send_command(command):
 		drone.move_forward(move_step)
 	elif (command=="back"):
 		drone.move_backward(move_step)
+	elif (command=="flip_f"):
+		drone.flip("forward")
+	elif (command=="flip_l"):
+		drone.flip("left")
+	elif (command=="flip_r"):
+		drone.flip("right")
+	elif (command=="flip_b"):
+		drone.flip("backward")
+	elif (command=="take_pic"):
+		take_picture()
+	elif (command=="video_start"):
+		start_video()
+	elif (command=="video_stop"):
+		stop_video()
+
+def speak(text):
+	global tts_engine
+	tts_engine.say(text)
+	tts_engine.runAndWait()
+	
+def take_picture():
+	global drone
+	global path_pictures
+	global with_detections_value
+	
+	while drone.read_frame() is not None:
+		frame=drone.read_frame()
+		if with_detections_value.get() == 1:
+			cuda_image=jetson.utils.cudaFromNumpy(frame)
+			detections = net.Detect(cuda_image)
+			frame=jetson.utils.cudaToNumpy(cuda_image)
+		write_image(path_pictures,frame)
+		messagebox.showinfo('Jetson Tools', 'Took image!')
+		break
+
+def start_video():
+	global recording
+	global recording_label_text
+	recording_label_text.set("Recording...")
+	recording=True
+	threading.Thread(target=record_frames).start()
+
+def record_frames():
+	global path_videos
+	global recording
+	global with_detections_value
+	global last_frame
+	global frame_resolution
+	
+	now = datetime.now() # current date and time
+	video_file='video_'+now.strftime("%Y%d%m%H%M%S")+'.avi'
+	out = cv2.VideoWriter(path_videos+video_file,cv2.VideoWriter_fourcc(*'DIVX'),30,frame_resolution)
+	if with_detections_value.get() == 0:
+		
+		while drone.read_frame() is not None:
+			frame=drone.read_frame()
+			out.write(frame)
+			sleep(0.01)
+			if not recording:
+				out.release()
+				break
+	else:
+		while last_frame is not None:
+			out.write(last_frame)
+			sleep(0.01)
+			if not recording:
+				out.release()
+				break
+
+def stop_video():
+	global recording
+	global recording_label_text
+	recording=False
+	recording_label_text.set("")
+	
+def write_image(image_path, image):
+	now = datetime.now() # current date and time
+	image_file='image_'+now.strftime("%Y%d%m%H%M%S")+'.jpg'
+	cv2.imwrite(image_path+image_file,image)
+
 
 def switch_camera():
-    global direction
-    global drone
-    direction+=1 
-    drone.send_command("downvision "+str(direction%2))
+	global direction
+	global drone
+	global frame_resolution
+	direction+=1
+	if direction%2 ==0:
+		frame_resolution=(960,720)
+	else:
+		frame_resolution=(320,240)
+	drone.send_command("downvision "+str(direction%2))
 
 # main window function
 def main():
@@ -366,10 +608,14 @@ def main():
 	global detection_object
 	global move_step
 	global angle_step
+	global net
+	global track_objects
 	
+	model_path=""
 	try:
 		settings_read=read_settings()
 		if settings_read:
+			#print(settings_data)
 			res = connect_to_tello(settings_data["tello_ip"])
 			detection_object = settings_data["object_to_track"]
 			pid.append(settings_data["p"])
@@ -377,6 +623,12 @@ def main():
 			pid.append(settings_data["d"])
 			move_step = settings_data["move_step"]
 			angle_step = settings_data["angle_step"]
+			if "custom_model_path" in settings_data:
+				model_path = settings_data["custom_model_path"]
+			else:
+				model_path=""				
+				
+			track_objects=settings_data["track_objects"]
 	except Exception as e:
 		print(e)
 		res=False
@@ -385,7 +637,7 @@ def main():
 	
 	root = tk.Tk()
 	root.title('Jetson Tools')
-
+	
 	root.geometry('960x720')
 	
 	menubar = tk.Menu(root)
@@ -418,6 +670,11 @@ def main():
 			
 			tello_status.config(fg="green")
 			tello_status['text']="Tello connected"
+			
+			if model_path=="":
+				net = jetson.inference.detectNet("ssd-mobilenet-v2")
+			else:
+				net = jetson.inference.detectNet(argv=['--model='+model_path+'/ssd-mobilenet.onnx','--labels='+model_path+'/labels.txt', '--input-blob=input_0', '--output-cvg=scores','--output-bbox=boxes'])
 			video_stream()
 	
 	
@@ -425,6 +682,7 @@ def main():
 	img2 = tk.PhotoImage(file='./assets/logo2.png')
 	root.tk.call('wm', 'iconphoto', root._w, img2)
 	
+	create_media_folder()
 	root.mainloop()
 
 
@@ -435,33 +693,40 @@ def video_stream():
 	global pid
 	global pError
 	global drone
-	
+	global net
+	global track_objects
+	global last_frame
 	
 	if drone.read_frame() is not None:
 		frame = drone.read_frame()
 		#print(frame.shape)
 		cuda_image=jetson.utils.cudaFromNumpy(frame)
 		
-		objectsListC = []
-		objectsArea = []
 		detections = net.Detect(cuda_image)
-		
-		for detection in detections:
-			cl_name=net.GetClassDesc(detection.ClassID)
-			center=detection.Center
+		if track_objects:
+			objectsListC = []
+			objectsArea = []
 			
-			if (cl_name==detection_object):
-				objectsListC.append(detection.Center)
-				objectsArea.append(detection.Area)
+			
+			for detection in detections:
+				cl_name=net.GetClassDesc(detection.ClassID)
+				center=detection.Center
+				
+				if (cl_name==detection_object):
+					objectsListC.append(detection.Center)
+					objectsArea.append(detection.Area)
+			
+			if len(objectsArea) != 0:
+				i = objectsArea.index(max(objectsArea))
+				pError = trackPerson(drone, objectsListC[i],720, pid, pError)
+			else:
+				pError=0
+				drone.joystick_control(0,0,0,0)
 		
-		if len(objectsArea) != 0:
-			i = objectsArea.index(max(objectsArea))
-			pError = trackPerson(drone, objectsListC[i],720, pid, pError)
-		else:
-			pError=0
-			drone.joystick_control(0,0,0,0)
-		
+			
 		frame_2=jetson.utils.cudaToNumpy(cuda_image)
+		last_frame=frame_2
+		
 		cv2image = cv2.cvtColor(frame_2, cv2.COLOR_BGR2RGBA)
 		img = Image.fromarray(cv2image)
 		imgtk = ImageTk.PhotoImage(image=img)
@@ -470,6 +735,8 @@ def video_stream():
 	lmain.after(1, video_stream) 
 
 connection_screen = tk.Tk()
+
+
 
 connection_screen.title('Jetson Tools')
    
@@ -485,7 +752,7 @@ connection_label.pack()
 img = tk.PhotoImage(file='./assets/logo.png')
 connection_screen.tk.call('wm', 'iconphoto', connection_screen._w, img)
 
-net = jetson.inference.detectNet("ssd-mobilenet-v2")
+
 # Set Interval
 connection_screen.after(1000,main)
 
@@ -493,6 +760,3 @@ connection_screen.after(1000,main)
  
 # Execute tkinter
 tk.mainloop()
-
-
-
